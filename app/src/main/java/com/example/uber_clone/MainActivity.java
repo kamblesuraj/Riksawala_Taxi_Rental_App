99e3cdde27e92ab34e1aa3607252803d9568e001
surajkamblesk <alphacoderofficial@gmail.com>
2019-08-21 21:33:16 +0530

    login screen

--- app/src/main/java/com/example/uber_clone/MainActivity.java
+++ app/src/main/java/com/example/uber_clone/MainActivity.java
@@ -8,9 +8,13 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
     Button cust_btn;
     Button driver_btn;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,24 +22,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         cust_btn = findViewById(R.id.customer_btn);
         driver_btn = findViewById(R.id.driver_btn);
+        mAuth = FirebaseAuth.getInstance();
 
         driver_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                open_new_activity();
+               open_driver_screen();
             }
         });
 
 
 
+        cust_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                open_customer_screen();
+            }
+        });
 
+        
 
     }
 
-    void open_new_activity()
+    void open_driver_screen()
     {
         Intent intent = new Intent(getApplicationContext(),DriverLogin.class);
         startActivity(intent);
 
     }
+    void open_customer_screen()
+    {
+        Intent intent = new Intent(getApplicationContext(),customer_login.class);
+        startActivity(intent);
+
+    }
+
 }
