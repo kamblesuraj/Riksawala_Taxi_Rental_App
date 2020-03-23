f2e63df6bd2cb60ba52e34a3758f0de7b338ee19
surajkamblesk <alphacoderofficial@gmail.com>
2020-03-23 14:09:30 +0530

    added map activity to the customer activity page

--- app/src/main/java/com/example/uber_clone/com/example/UberClone/customer/Map_Customer.java
+++ app/src/main/java/com/example/uber_clone/com/example/UberClone/customer/Map_Customer.java
@@ -1,15 +1,29 @@
 package com.example.uber_clone.com.example.UberClone.customer;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.uber_clone.R;
 import com.example.uber_clone.com.example.UberClone.driver.DriverLogin;
 import com.example.uber_clone.com.example.UberClone.driver.Map_Driver;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -23,7 +37,10 @@ import java.util.Objects;
 public class Map_Customer extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-     Button logout_btn;
+    Button logout_btn;
+    private Location mLastLocation;
+    private FusedLocationProviderClient mFusedLocationClient;
+    LocationRequest mLocationRequest;
 
 
     @Override
@@ -41,11 +58,9 @@ public class Map_Customer extends FragmentActivity implements OnMapReadyCallback
         assert mapFragment != null;
         Objects.requireNonNull(mapFragment).getMapAsync(this);
 
-        logout_btn.setOnClickListener(new View.OnClickListener()
-        {
+        logout_btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 FirebaseAuth.getInstance().signOut();
                 Intent intent = new Intent(Map_Customer.this, customer_login.class);
                 startActivity(intent);
@@ -68,9 +83,83 @@ public class Map_Customer extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+        } else {
+            checkLocationPermission();
+
+        }
     }
+
+    LocationCallback mLocationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            for (Location location : locationResult.getLocations()) {
+                if (location != null) {
+                    mLastLocation = location;
+                    LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                    mMap.animateCamera(CameraUpdateFactory.zoomTo(18));
+
+                    GetSpeedAndAccuracy(location);
+
+                }
+            }
+        }
+    };
+
+
+    private void checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                new AlertDialog.Builder(this)
+                        .setTitle("Grant Location Permissions")
+                        .setMessage("Please Grant Location Permission, Or Else App Might Not Work")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                ActivityCompat.requestPermissions(Map_Customer.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                            }
+                        })
+                        .create()
+                        .show();
+            } else {
+                ActivityCompat.requestPermissions(Map_Customer.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+                    mMap.setMyLocationEnabled(true);
+                }
+            } else {
+                Toast.makeText(getApplicationContext(), "Please provide the permission", Toast.LENGTH_LONG).show();
+            }
+
+        }
+    }
+
+    private void GetSpeedAndAccuracy(Location location) {
+        float Accuracy = location.getAccuracy();
+        float speed = location.getSpeed();
+
+        Toast.makeText(Map_Customer.this, "Accuracy is :" + Accuracy, Toast.LENGTH_SHORT).show();
+        Toast.makeText(Map_Customer.this, "Speed is :" + speed, Toast.LENGTH_SHORT).show();
+
+    }
+
+
 }
